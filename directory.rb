a54859cfaca98771cffb9f72c171a040ef13e905
Rosie <r.gollancz@gmail.com>
2016-09-12 11:09:14 +0100

    Add comments to student directory

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+# then we print the total number of students
 print "Overall, we have "
+# nb - the print method does not add a new line character
 print 11
 puts " great students"
