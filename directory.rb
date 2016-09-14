16143cb6e6487025d6c28cdad1e55fa4db4e273e
Rosie <r.gollancz@gmail.com>
2016-09-14 09:52:27 +0100

    Refactor interactive menu - separate methods

--- directory.rb
+++ directory.rb
@@ -1,9 +1,9 @@
+@students = []
+
 # get student info from user
 def input_students
   puts "Please enter the student's information"
   puts "When you're done entering information, just hit return twice"
-  # create an empty array
-  students = []
 
   while true
     puts "Enter full name:"
@@ -34,12 +34,11 @@ def input_students
         if answer == 'x'
           puts "Student information deleted!"
         else
-          students << {name: name.to_sym, cohort: cohort.to_sym, hobby: hobby.to_sym, location: location.to_sym}
+          @students << {name: name.to_sym, cohort: cohort.to_sym, hobby: hobby.to_sym, location: location.to_sym}
           puts "Student information saved!"
         end
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{@students.count} students"
   end
-  students
 end
 
 def print_header
@@ -47,24 +46,22 @@ def print_header
   puts "-------------".center(100)
 end
 
-# Task: rewrite the each() method that prints all students using while or until control flow methods
-def print_students(students)
+def print_students_list
   # set a value that we will set the method to loop up until - i.e. the number of students in the students array
-  print_total = students.length
+  print_total = @students.count
   # set a counter to increase during each loop
   count = 0
   # keep looping until the print_total has been reached - i.e. until every element in the array has been run through
   while count < print_total
     # set the student variable to the item in the array (the hash) to be accessed, as determined by the counter
-    student = students[count]
+    student = @students[count]
     puts "#{student[:name]} (#{student[:cohort]} cohort, main hobby: #{student[:hobby]}, location: #{student[:location]})".center(100)
     # increase
     count += 1
   end
 end
 
-# Task: print students grouped by cohorts
-def print_cohorts(students)
+def print_cohorts
   # create list of existing cohorts - method could be improved by creating this list automatically from the directory
   cohorts = ['February 2016','April 2016','September 2016']
   # iterate through the cohorts array one cohort at a time
@@ -72,7 +69,7 @@ def print_cohorts(students)
     # convert the month to a symbol (since we're doing that during the input stage)
     month = month.to_sym
     # iterate through the students array
-    students.find do |student|
+    @students.find do |student|
       # if the student's cohort matches the current month in the cohort array, puts the student's information
       if student[:cohort] == month
         puts "#{student[:name]} (#{student[:cohort]} cohort), main hobby: #{student[:hobby]}, location: #{student[:location]})".center(100)
@@ -83,36 +80,44 @@ def print_cohorts(students)
   end
 end
 
-def print_footer(names)
-  if names.length == 1
-    puts  "Overall, we have #{names.count} great student".center(100)
+def print_footer
+  if @students.length == 1
+    puts  "Overall, we have #{@students.count} great student".center(100)
   else
-    puts "Overall, we have #{names.count} great students".center(100)
+    puts "Overall, we have #{@students.count} great students".center(100)
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Display the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean. Please try again."
+  end
+end
+
+
 def interactive_menu
-  students = []
   loop do
-    # print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Display the students"
-    puts "9. Exit"
-    # read the input and save it into a variable
-    selection = gets.chomp
-    # do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print_students(students)
-      print_footer
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, please try again."
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
